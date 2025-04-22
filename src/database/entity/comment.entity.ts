00f6693cbda0aa8b882b07624d63fc62f9f9be2e
kangjuhyup <fog0510@gmail.com>
2025-04-22 21:48:29 +0900

    feat : 게시글 & 댓글 Entity

---
+++ src/database/entity/comment.entity.ts
@@ -0,0 +1,48 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from "typeorm";
+import { PostEntity } from "./post.entity";
+import { DefaultEntity } from "./default.entity";
+
+@Entity()
+export class CommentEntity extends DefaultEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    content: string;
+
+    @Column()
+    author: string;
+
+    @Column()
+    postId: number;
+
+    @Column()
+    parentId : number;
+
+    @OneToMany(() => CommentEntity, comment => comment.parentId, { nullable: true })
+    comments?: CommentEntity[];
+
+    @ManyToOne(() => CommentEntity, comment => comment.comments)
+    @JoinColumn({ name: 'parentId' })
+    parent?: CommentEntity;
+
+    @ManyToOne(() => PostEntity, post => post.comments)
+    @JoinColumn({ name: 'postId' })
+    post: PostEntity;
+
+    static of(param : {
+        content : string,
+        author : string
+    }) {
+        const comment = new CommentEntity();
+        comment.content = param.content;
+        comment.author = param.author;
+        return comment;
+    }
+
+    softDelete() {
+        this.deletedAt = new Date();
+    }
+    
+}
---
+++ src/database/entity/post.entity.ts
@@ -0,0 +1,58 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import * as bcrypt from "bcrypt"; // 비밀번호 해싱을 위한 bcrypt 모듈 추가
+import { CommentEntity } from "./comment.entity";
+import { DefaultEntity } from "./default.entity";
+
+@Entity()
+export class PostEntity extends DefaultEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    author: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => CommentEntity, comment => comment.post, { nullable: true })
+    comments?: CommentEntity[];
+    
+    static of(param : {
+        title : string,
+        content : string,
+        author : string,
+        password : string
+    }) {
+        const post = new PostEntity();
+        post.title = param.title;
+        post.content = param.content;
+        post.author = param.author;
+        post.setPassword(param.password);
+        return post;
+    }   
+
+    private async setPassword(password: string) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(password, saltRounds);
+    }
+
+    async checkPassword(plainPassword: string): Promise<boolean> {
+        return await bcrypt.compare(plainPassword, this.password);
+    }
+
+    async modify(param : {
+        title? : string,
+        content? : string
+    }) {
+        this.updatedAt = new Date();
+        if(param.title) this.title = param.title;
+        if(param.content) this.content = param.content;
+    }
+}
