d69afc7a2a1ea55b88fff2b5d50253440cd5978e
kangjuhyup <fog0510@gmail.com>
2025-04-23 20:31:14 +0900

    feat(app) : 모듈 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // TypeORM 모듈 import
 import { typeOrmConfig } from './database/datasource/mariadb.ds'; // 마리아DB 연결 옵션 import
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostEntity } from './database/entity/post.entity';
-import { CommentEntity } from './database/entity/comment.entity';
-import { TransactionService } from './database/transaction/transaction.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventModule } from './domain/event.module';
+import { RouterModule } from './domain/router.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { GlobalExceptionInterceptor } from '@/common/interceptor/global.exception.interceptor';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TypeOrmModule.forFeature([PostEntity, CommentEntity]), // 엔티티 등록
+    EventEmitterModule.forRoot(),
+    EventModule,
+    RouterModule,
   ],
   controllers: [AppController],
-  providers: [AppService, TransactionService], // 트랜잭션 서비스 등록
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: GlobalExceptionInterceptor },
+  ],
 })
 export class AppModule {}
