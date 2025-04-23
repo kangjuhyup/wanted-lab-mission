032c1d1c655857016d755c9248f990988ad414fc
kangjuhyup <fog0510@gmail.com>
2025-04-23 22:12:40 +0900

    feat(database) : entity

--- src/database/datasource/mariadb.ds.ts
+++ src/database/datasource/mariadb.ds.ts
@@ -2,6 +2,10 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { PostEntity } from '../entity/post.entity';
 import { CommentEntity } from '../entity/comment.entity';
 import * as dotenv from 'dotenv';
+import { NotificationEntity } from '../entity/notification.entity';
+import { NotificationKeywordEntity } from '../entity/notification.keyword.entity';
+import { NotificationTypeEntity } from '../entity/notification.type.entity';
+import { KeywordEntity } from '../entity/keyword.entity';
 
 dotenv.config();
 
@@ -13,7 +17,7 @@ export const typeOrmConfig: DataSourceOptions = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [PostEntity, CommentEntity],
+    entities : [PostEntity, CommentEntity, NotificationEntity, NotificationKeywordEntity, NotificationTypeEntity, KeywordEntity],
     logging: process.env.NODE_ENV === 'development',
     migrations: [__dirname + '/../migration/*.{js,ts}'],
 };
